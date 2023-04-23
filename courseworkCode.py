66fc63ef3b3ac222cff4c9c13360ffe048968f63
Dbeee-Acharya <dbeee.acharya@gmail.com>
2023-04-23 19:52:55 +0545

    Error handling for UserChoiceWelcomeScreen

--- courseworkCode.py
+++ courseworkCode.py
@@ -1,5 +1,8 @@
 # Python coursework dibbeshwor acharya
 
+import sys # gives us access to variables used or maintained by the interpreter. Used in this code to call sys.exit() which closes the program
+
+
 def decorationStar():
     return ("**********************************************************************")
 
@@ -9,28 +12,47 @@ def decorationDash():
 
 
 # Welcome screen section
-print(decorationStar())
-print("\t\t\tLaptop Sales/Purchase")
-print(decorationStar())
-print(decorationDash())
+def welcomeScreenMain():
+    print(decorationStar())
+    print("\t\t\tLaptop Sales/Purchase")
+    print(decorationStar())
+    print(decorationDash())
 
-print("Please choose the required option")
-print("\n")
+    print("Please choose the required option")
+    print("\n")
 
-print("1. Laptop Sales")
-print("2. Laptop Purcahse")
-print("3. Exit the program")
-print(decorationDash())
-print(decorationStar())
-print("\n")
+    print("1. Laptop Sales")
+    print("2. Laptop Purcahse")
+    print("3. Current Stock")
+    print("4. Exit the program")
+    print(decorationDash())
+    print(decorationStar())
+    print("\n")
 
-while True:
-    try:
-        userChoiceWelcomeScreen = int(input("Your choice: "))
-        break
-    except ValueError:
-        print("\nSelection Invalid, please try again!\n")
-        print(decorationDash())
+    # Takes userInput for desired choice. Any out of bounds choice or Invalid choice is error handled
+    while True:
+        try:
+            userChoiceWelcomeScreen = int(input("Your choice: "))
+
+            if userChoiceWelcomeScreen < 1 or userChoiceWelcomeScreen > 4:
+                print("\nSelection Invalid, please try again!\n")
+                print(decorationDash())
+                continue
+
+            break
+
+        except ValueError:
+            print("\nSelection Invalid, please try again!\n")
+            print(decorationDash())
+
+    if userChoiceWelcomeScreen == 1:
+        getLaptopStock()
+    elif userChoiceWelcomeScreen == 2:
+        sellLaptop()
+    elif userChoiceWelcomeScreen == 3:
+        getLaptopStock()
+    else:
+        sys.exit("\nProgram Closed\n")
 
 
 def getLaptopStock():
@@ -55,3 +77,6 @@ def sellLaptop():
 def purchaseLaptop():
     # to purchase laptop
     return
+
+
+welcomeScreenMain()
