d52aedc4af0f5ffedbbdf8c17486c4d15f5ba0fe
Dbeee-Acharya <dbeee.acharya@gmail.com>
2023-04-23 09:32:02 +0545

    Complete welcome screen

---
+++ courseworkCode.py
@@ -0,0 +1,24 @@
+# Python coursework dibbeshwor acharya
+
+# Welcome screen section
+print("**********************************************************************")
+print("\t\t\tLaptop Sales/Purchase")
+print("**********************************************************************")
+print("----------------------------------------------------------------------")
+
+print("Please choose the required option")
+print("\n")
+
+print("1. Laptop Sales")
+print("2. Laptop Purcahse")
+print("3. Exit the program")
+print("----------------------------------------------------------------------")
+print("**********************************************************************")
+print("\n")
+
+userChoiceWelcomeScreen = int(input("Your choice: "))
+
+
+# Laptop Sale
+
+# Laptop Purchase
